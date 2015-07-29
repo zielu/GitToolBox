d96e4a69d7b9f63ce1a67f0ca6cebabdde1f559b
Lukasz Zielinski <zieluuuu@gmail.com>
2015-07-29 21:51:28 +0200

    Tweaked no remote status display

--- GitToolBox/src/zielu/gittoolbox/ui/StatusText.java
+++ GitToolBox/src/zielu/gittoolbox/ui/StatusText.java
@@ -3,12 +3,13 @@ package zielu.gittoolbox.ui;
 import org.jetbrains.annotations.NotNull;
 import zielu.gittoolbox.ResBundle;
 import zielu.gittoolbox.status.GitAheadBehindCount;
+import zielu.gittoolbox.status.Status;
 
 public enum StatusText {
     ;
 
     public static String format(@NotNull GitAheadBehindCount aheadBehind) {
-        if (aheadBehind.status().isValid()) {
+        if (aheadBehind.status() == Status.Success) {
             return StatusMessages.getInstance().aheadBehindStatus(aheadBehind);
         } else {
             return ResBundle.getString("git.na");
@@ -16,7 +17,7 @@ public enum StatusText {
     }
 
     public static String formatToolTip(@NotNull GitAheadBehindCount aheadBehind) {
-        if (aheadBehind.status().isValid()) {
+        if (aheadBehind.status() == Status.Success) {
             return "";
         } else {
             return StatusMessages.getInstance().aheadBehindStatus(aheadBehind);
