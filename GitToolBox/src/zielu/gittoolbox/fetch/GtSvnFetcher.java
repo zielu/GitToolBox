01d65405b7af81fb4cf26616419c7e7625397b7d
Lukasz Zielinski <zieluuuu@gmail.com>
2015-12-19 09:20:43 +0100

    git svn support

---
+++ GitToolBox/src/zielu/gittoolbox/fetch/GtSvnFetcher.java
@@ -0,0 +1,24 @@
+package zielu.gittoolbox.fetch;
+
+import com.google.common.base.Optional;
+import com.intellij.openapi.project.Project;
+import git4idea.repo.GitRepository;
+import git4idea.update.GitFetchResult;
+import org.jetbrains.annotations.NotNull;
+import zielu.gittoolbox.repo.GtConfig;
+import zielu.gittoolbox.repo.GtRepositoryManager;
+
+public class GtSvnFetcher {
+    private final Project myProject;
+
+    public GtSvnFetcher(Project project) {
+        myProject = project;
+    }
+
+    public GitFetchResult fetch(@NotNull GitRepository repository) {
+        GtRepositoryManager manager = GtRepositoryManager.getInstance(myProject);
+        Optional<GtConfig> config = manager.configFor(repository);
+
+        return GitFetchResult.success();
+    }
+}
---
+++ GitToolBox/src/zielu/gittoolbox/repo/GtRepositoryManager.java
@@ -0,0 +1,52 @@
+package zielu.gittoolbox.repo;
+
+import com.google.common.base.Optional;
+import com.intellij.openapi.components.AbstractProjectComponent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.util.containers.ConcurrentHashMap;
+import com.intellij.util.messages.MessageBusConnection;
+import git4idea.repo.GitRepository;
+import git4idea.repo.GitRepositoryChangeListener;
+import java.io.File;
+import java.util.Map;
+import org.jetbrains.annotations.NotNull;
+
+public class GtRepositoryManager extends AbstractProjectComponent implements GitRepositoryChangeListener {
+    private final Map<GitRepository, GtConfig> myConfigs = new ConcurrentHashMap<GitRepository, GtConfig>();
+    private MessageBusConnection myConnection;
+
+    public GtRepositoryManager(Project project) {
+        super(project);
+    }
+
+    @Override
+    public void repositoryChanged(@NotNull GitRepository repository) {
+        File configFile = new File(VfsUtilCore.virtualToIoFile(repository.getGitDir()), "config");
+        GtConfig config = GtConfig.load(configFile);
+        myConfigs.put(repository, config);
+    }
+
+    public Optional<GtConfig> configFor(GitRepository repository) {
+        return Optional.fromNullable(myConfigs.get(repository));
+    }
+
+    @Override
+    public void initComponent() {
+        myConnection = myProject.getMessageBus().connect();
+        myConnection.subscribe(GitRepository.GIT_REPO_CHANGE, this);
+    }
+
+    @Override
+    public void disposeComponent() {
+        if (myConnection != null) {
+            myConnection.disconnect();
+            myConnection = null;
+        }
+        myConfigs.clear();
+    }
+
+    public static GtRepositoryManager getInstance(@NotNull Project project) {
+        return project.getComponent(GtRepositoryManager.class);
+    }
+}
