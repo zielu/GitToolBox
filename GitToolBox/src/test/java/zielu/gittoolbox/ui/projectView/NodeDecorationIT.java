12436d8129a40d3bd4aba50521591ae9b6e36856
Lukasz Zielinski <zieluuuu@gmail.com>
2017-11-16 00:01:24 +0100

    Disabled IT test

--- GitToolBox/src/test/java/zielu/gittoolbox/ui/projectView/NodeDecorationIT.java
+++ GitToolBox/src/test/java/zielu/gittoolbox/ui/projectView/NodeDecorationIT.java
@@ -1,11 +1,20 @@
 package zielu.gittoolbox.ui.projectView;
 
+import com.google.common.base.Charsets;
+import com.intellij.dvcs.repo.VcsRepositoryManager;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.testFramework.LightProjectDescriptor;
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
 import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
 import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
 import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
 import com.intellij.testFramework.fixtures.impl.LightTempDirTestFixtureImpl;
+import com.intellij.vcsUtil.VcsUtil;
+import git4idea.GitVcs;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.dircache.DirCache;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -13,13 +22,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import zielu.intellij.junit5.ContainsTempFiles;
 import zielu.intellij.junit5.IdeaLightExtension;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 @ExtendWith(IdeaLightExtension.class)
 @ContainsTempFiles
 class NodeDecorationIT {
-
+    private static Path myTestDataPath;
     private static CodeInsightTestFixture myFixture;
+    private static Module myModule;
+    private static Project myProject;
 
     static LightProjectDescriptor getDescriptor() {
         LightProjectDescriptor descriptor = new DefaultLightProjectDescriptor();
@@ -31,7 +45,18 @@ class NodeDecorationIT {
         myFixture = IdeaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(fixture, new LightTempDirTestFixtureImpl(true));
 
         myFixture.setUp();
-        myFixture.setTestDataPath(Paths.get(".", "testRun", "it").toAbsolutePath().toString());
+        myTestDataPath = Paths.get(".", "testRun", "it", NodeDecorationIT.class.getSimpleName());
+        initGit(myTestDataPath);
+        myFixture.setTestDataPath(myTestDataPath.toAbsolutePath().toString());
+        myModule = myFixture.getModule();
+        myProject = myFixture.getProject();
+    }
+
+    private static void initGit(Path testDataDir) throws Exception {
+        Git git = Git.init().setDirectory(testDataDir.toFile()).call();
+        Files.write(testDataDir.resolve("file.txt"), Arrays.asList("abc"), Charsets.UTF_8);
+        git.add().addFilepattern("file.txt").call();
+        git.commit().setMessage("Initial commit").call();
     }
 
     @AfterAll
@@ -41,11 +66,14 @@ class NodeDecorationIT {
         }
         finally {
             myFixture = null;
+            myModule = null;
         }
     }
 
     @Test
     void test() {
-
+        ProjectLevelVcsManager vcsManager = ProjectLevelVcsManager.getInstance(myProject);
+        /*vcsManager.setDirectoryMappings(VcsUtil.addMapping(vcsManager.getDirectoryMappings(),
+                myTestDataPath.toAbsolutePath().toString(), GitVcs.NAME));*/
     }
 }
