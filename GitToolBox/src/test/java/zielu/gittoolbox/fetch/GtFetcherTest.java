6185a4caa86c782545119ae0cdabe92058602562
Lukasz Zielinski <zieluuuu@gmail.com>
2018-10-01 22:56:22 +0200

    Limit parallel auto-fetch task count (#103)

--- GitToolBox/src/test/java/zielu/gittoolbox/fetch/GtFetcherTest.java
+++ GitToolBox/src/test/java/zielu/gittoolbox/fetch/GtFetcherTest.java
@@ -1,12 +1,15 @@
 package zielu.gittoolbox.fetch;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.google.common.collect.Lists;
 import com.intellij.openapi.progress.ProgressIndicator;
 import git4idea.repo.GitRepository;
 import git4idea.update.GitFetchResult;
@@ -20,7 +23,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 import zielu.gittoolbox.metrics.MockMetrics;
 
 @Tag("fast")
@@ -50,7 +55,7 @@ class GtFetcherTest {
     fetcher = GtFetcher.builder()
         .withClient(client)
         .withMetrics(new MockMetrics())
-        .withExecutor(Executors.newCachedThreadPool())
+        .withExecutor(Executors.newFixedThreadPool(4))
         .withUi(ui)
         .build(indicator);
   }
@@ -80,7 +85,38 @@ class GtFetcherTest {
     Collection<GitRepository> repositories = fetcher.fetchRoots(Collections.singletonList(repository));
 
     assertThat(repositories).isEmpty();
+    checkIndicatorDone();
+  }
+
+  private void checkIndicatorDone() {
     verify(indicator, atLeastOnce()).setFraction(indicatorCaptor.capture());
     assertThat(indicatorCaptor.getAllValues()).endsWith(1.0);
+  }
+
+  @Test
+  void fetchRootsShouldHandleMultipleRepos() {
+    GitRepository repo2 = mock(GitRepository.class);
+    GitRepository repo3 = mock(GitRepository.class);
+    GitRepository repo4 = mock(GitRepository.class);
+    when(client.fetch(any())).thenAnswer(new Answer<Object>() {
+      @Override
+      public Object answer(InvocationOnMock invocation) throws Throwable {
+        GitRepository repo = invocation.getArgument(0);
+        if (repo.equals(repository)) {
+          throw new RuntimeException("Test error 1");
+        } else if (repo.equals(repo3)) {
+          return GitFetchResult.error(new Exception("Test error 2"));
+        } else if (repo.equals(repo2) || repo.equals(repo4)) {
+          return GitFetchResult.success();
+        } else {
+          fail("Unsupported repo");
+          return null;
+        }
+      }
+    });
+
+    Collection<GitRepository> repositories = fetcher.fetchRoots(Lists.newArrayList(repository, repo2, repo3, repo4));
+    assertThat(repositories).containsOnly(repo2, repo4);
+    checkIndicatorDone();
   }
 }
