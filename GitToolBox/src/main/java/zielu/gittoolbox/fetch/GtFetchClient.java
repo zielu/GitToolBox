d18b55e9f5a69efb02cfaa75cd41e7a408d08c66
Lukasz Zielinski <zieluuuu@gmail.com>
2019-02-11 20:06:49 +0100

    Removed dead code

--- GitToolBox/src/main/java/zielu/gittoolbox/fetch/GtFetchClient.java
+++
@@ -1,8 +0,0 @@
-package zielu.gittoolbox.fetch;
-
-import git4idea.repo.GitRepository;
-import org.jetbrains.annotations.NotNull;
-
-interface GtFetchClient {
-  GtFetchResult fetch(@NotNull GitRepository repository);
-}
