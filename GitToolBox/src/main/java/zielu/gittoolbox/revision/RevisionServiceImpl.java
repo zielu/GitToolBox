98148b099449971b5a514c435bfc0730147baa55
Lukasz Zielinski <zieluuuu@gmail.com>
2019-03-12 07:13:15 +0100

    Remove guava cache metrics

--- GitToolBox/src/main/java/zielu/gittoolbox/revision/RevisionServiceImpl.java
+++ GitToolBox/src/main/java/zielu/gittoolbox/revision/RevisionServiceImpl.java
@@ -1,5 +1,7 @@
 package zielu.gittoolbox.revision;
 
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Timer;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.intellij.openapi.Disposable;
@@ -10,8 +12,8 @@ import com.intellij.openapi.vcs.history.VcsRevisionNumber;
 import com.intellij.openapi.vfs.VirtualFile;
 import java.time.Duration;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
 import org.jetbrains.annotations.NotNull;
-import zielu.gittoolbox.metrics.GuavaCacheMetrics;
 import zielu.gittoolbox.metrics.ProjectMetrics;
 
 class RevisionServiceImpl implements RevisionService, Disposable {
@@ -20,16 +22,21 @@ class RevisionServiceImpl implements RevisionService, Disposable {
   private final Cache<VcsRevisionNumber, RevisionEntry> cache = CacheBuilder.newBuilder()
       .maximumSize(500)
       .expireAfterAccess(Duration.ofMinutes(30))
-      .recordStats()
       .build();
   private final RevisionServiceGateway gateway;
   private final RevisionInfoFactory infoFactory;
+  private final Counter invalidatedCounter;
+  private final Timer loadFileTimer;
+  private final Timer loadLineTimer;
 
   RevisionServiceImpl(@NotNull RevisionServiceGateway gateway, @NotNull RevisionInfoFactory infoFactory,
                       @NotNull ProjectMetrics metrics) {
     this.gateway = gateway;
     this.infoFactory = infoFactory;
-    metrics.addAll(new GuavaCacheMetrics(cache, "revision-cache"));
+    metrics.gauge("revision-cache.size", cache::size);
+    invalidatedCounter = metrics.counter("revision-cache.invalidated-count");
+    loadFileTimer = metrics.timer("revision-cache.load-file");
+    loadLineTimer = metrics.timer("revision-cache.load-line");
     this.gateway.disposeWithProject(this);
   }
 
@@ -45,7 +52,7 @@ class RevisionServiceImpl implements RevisionService, Disposable {
     if (lineRevision != null) {
       RevisionEntry entry = loadEntry(annotation, lineRevision, lineNumber);
       if (entry.partial) {
-        cache.invalidate(lineRevision);
+        invalidate(lineRevision);
         entry = loadEntry(annotation, lineRevision, lineNumber);
         gateway.fireRevisionUpdated(entry.info);
       }
@@ -57,20 +64,31 @@ class RevisionServiceImpl implements RevisionService, Disposable {
   private RevisionEntry loadEntry(@NotNull FileAnnotation annotation, @NotNull VcsRevisionNumber lineRevision,
                                   int lineNumber) {
     try {
-      return cache.get(lineRevision, () -> new RevisionEntry(
-          infoFactory.forLine(annotation, lineRevision, lineNumber), false));
+      return cache.get(lineRevision, () ->
+          loadEntry(loadLineTimer,
+              () -> new RevisionEntry(infoFactory.forLine(annotation, lineRevision, lineNumber), false)));
     } catch (ExecutionException e) {
       log.warn("Failed to load revision " + lineRevision + " for line " + lineNumber);
       return EMPTY;
     }
   }
 
+  @NotNull
+  private RevisionEntry loadEntry(Timer timer, Supplier<RevisionEntry> loader) {
+    return timer.timeSupplier(loader);
+  }
+
+  private void invalidate(@NotNull VcsRevisionNumber revisionNumber) {
+    cache.invalidate(revisionNumber);
+    invalidatedCounter.inc();
+  }
+
   @Override
   public RevisionInfo getForFile(@NotNull VirtualFile file, @NotNull VcsFileRevision revision) {
     if (revision != VcsFileRevision.NULL) {
       try {
-        RevisionEntry entry = cache.get(revision.getRevisionNumber(),
-            () -> new RevisionEntry(infoFactory.forFile(file, revision), true));
+        RevisionEntry entry = cache.get(revision.getRevisionNumber(), () ->
+            loadEntry(loadFileTimer, () -> new RevisionEntry(infoFactory.forFile(file, revision), true)));
         return entry.info;
       } catch (ExecutionException e) {
         log.warn("Failed to load revision " + revision +  " for " + file);
