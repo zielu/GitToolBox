308484d11dfea816f6432aeef301a73109ef65c7
Lukasz Zielinski <zieluuuu@gmail.com>
2019-06-05 21:45:12 +0200

    Run status bar fetch in background (#154)
    (cherry picked from commit 8364f76a4aef4f452ce57eb5cdcd6aed4d546a7a)

--- GitToolBox/src/main/java/zielu/gittoolbox/ui/statusbar/actions/FetchAction.java
+++ GitToolBox/src/main/java/zielu/gittoolbox/ui/statusbar/actions/FetchAction.java
@@ -1,11 +1,19 @@
 package zielu.gittoolbox.ui.statusbar.actions;
 
+import com.google.common.base.Preconditions;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.project.Project;
+import git4idea.GitVcs;
 import git4idea.i18n.GitBundle;
 import git4idea.repo.GitRepository;
 import org.jetbrains.annotations.NotNull;
+import zielu.gittoolbox.ResBundle;
 import zielu.gittoolbox.fetch.GtFetchUtil;
+import zielu.gittoolbox.ui.util.AppUiUtil;
+import zielu.gittoolbox.util.GtUtil;
 
 public class FetchAction extends DumbAwareAction {
   private final GitRepository repository;
@@ -16,7 +24,19 @@ public class FetchAction extends DumbAwareAction {
   }
 
   @Override
-  public void actionPerformed(AnActionEvent e) {
-    GtFetchUtil.fetch(repository);
+  public void actionPerformed(@NotNull AnActionEvent e) {
+    Project project = repository.getProject();
+    AppUiUtil.invokeLaterIfNeeded(project, this::fetch);
+  }
+
+  private void fetch() {
+    Project project = repository.getProject();
+    GitVcs.runInBackground(new Task.Backgroundable(Preconditions.checkNotNull(project),
+        ResBundle.message("message.fetching", GtUtil.name(repository))) {
+      @Override
+      public void run(@NotNull ProgressIndicator indicator) {
+        GtFetchUtil.fetch(repository);
+      }
+    });
   }
 }
