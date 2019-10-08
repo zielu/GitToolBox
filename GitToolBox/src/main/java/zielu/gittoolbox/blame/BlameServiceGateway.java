02cadefc3d93a745ad9e8ad0fd5fe6dfba62df95
Lukasz Zielinski <zieluuuu@gmail.com>
2019-10-09 00:42:32 +0200

    Convert dependencies injection into local gateway

--- GitToolBox/src/main/java/zielu/gittoolbox/blame/BlameServiceGateway.java
+++ GitToolBox/src/main/java/zielu/gittoolbox/blame/BlameServiceGateway.java
@@ -4,36 +4,24 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.localVcs.UpToDateLineNumberProvider;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.VcsException;
-import com.intellij.openapi.vcs.history.VcsFileRevision;
 import com.intellij.openapi.vcs.impl.UpToDateLineNumberProviderImpl;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.messages.MessageBus;
-import git4idea.GitVcs;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import zielu.gittoolbox.util.DisposeSafeRunnable;
 import zielu.gittoolbox.util.GatewayBase;
-import zielu.gittoolbox.util.GtUtil;
 
 class BlameServiceGateway extends GatewayBase {
-  private final GitVcs git;
   private final MessageBus messageBus;
 
   BlameServiceGateway(@NotNull Project project) {
     super(project);
-    git = GitVcs.getInstance(project);
     messageBus = project.getMessageBus();
   }
 
   @NotNull
   UpToDateLineNumberProvider createUpToDateLineProvider(@NotNull Document document) {
     return new UpToDateLineNumberProviderImpl(document, project);
-  }
-
-  @Nullable
-  VcsFileRevision getLastRevision(@NotNull VirtualFile file) throws VcsException {
-    return git.getVcsHistoryProvider().getLastRevision(GtUtil.localFilePath(file));
   }
 
   void fireBlameUpdated(@NotNull VirtualFile file) {
---
+++ GitToolBox/src/main/kotlin/zielu/gittoolbox/blame/BlameLoaderLocalGateway.kt
@@ -0,0 +1,22 @@
+package zielu.gittoolbox.blame
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import git4idea.GitVcs
+import git4idea.repo.GitRepository
+import zielu.gittoolbox.cache.VirtualFileRepoCache
+import zielu.gittoolbox.revision.RevisionService
+
+class BlameLoaderLocalGateway(val project: Project) {
+  fun getRepoForFile(vFile: VirtualFile): GitRepository? {
+    return VirtualFileRepoCache.getInstance(project).getRepoForFile(vFile)
+  }
+
+  fun getRevisionService(): RevisionService {
+    return RevisionService.getInstance(project)
+  }
+
+  fun getGitVcs(): GitVcs {
+    return GitVcs.getInstance(project)
+  }
+}
