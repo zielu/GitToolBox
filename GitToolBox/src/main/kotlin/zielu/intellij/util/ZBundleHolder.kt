8bbb5b39ca6aa02956346c3eb967d0116d5da801
Lukasz Zielinski <zieluuuu@gmail.com>
2019-10-14 21:23:01 +0200

    Use lazy instead of custom loading

--- GitToolBox/src/main/kotlin/zielu/intellij/util/ZBundleHolder.kt
+++ GitToolBox/src/main/kotlin/zielu/intellij/util/ZBundleHolder.kt
@@ -3,12 +3,7 @@ package zielu.intellij.util
 import java.util.ResourceBundle
 
 class ZBundleHolder(private val bundleName: String) {
-  private var bundle: ResourceBundle? = null
-
-  fun getBundle(): ResourceBundle {
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(bundleName)
-    }
-    return checkNotNull(bundle) { "Failed to load resource bundle $bundleName" }
+  val bundle: ResourceBundle by lazy {
+    ResourceBundle.getBundle(bundleName)
   }
 }
