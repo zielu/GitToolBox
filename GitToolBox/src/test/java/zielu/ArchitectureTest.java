8c6d328d85abe1937d1aa3a4aab3807506c030b0
Lukasz Zielinski <zieluuuu@gmail.com>
2020-01-10 12:53:26 +0100

    Prepare for ArchUnit

---
+++ GitToolBox/src/test/java/zielu/ArchitectureTest.java
@@ -0,0 +1,12 @@
+package zielu;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+//@Tag("architecture")
+public @interface ArchitectureTest {
+}
