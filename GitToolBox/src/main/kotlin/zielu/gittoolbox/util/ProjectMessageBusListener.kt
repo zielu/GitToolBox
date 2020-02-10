725428016c355ffa199ce2d99dd19019131ab261
Lukasz Zielinski <zieluuuu@gmail.com>
2020-02-10 21:24:32 +0100

    Fix PluginException: Cannot create ServiceAdapter (#198)

--- GitToolBox/src/main/kotlin/zielu/gittoolbox/util/ProjectMessageBusListener.kt
+++ GitToolBox/src/main/kotlin/zielu/gittoolbox/util/ProjectMessageBusListener.kt
@@ -1,12 +1,17 @@
 package zielu.gittoolbox.util
 
 import com.intellij.openapi.project.Project
+import org.slf4j.LoggerFactory
 
 internal abstract class ProjectMessageBusListener(private val project: Project) {
+  private val log = LoggerFactory.getLogger(this::class.java)
+
   fun handleEvent(handler: (project: Project) -> Unit) {
     val isActive = !AppUtil.runReadAction { project.isDisposedOrDisposeInProgress }
     if (isActive) {
       handler.invoke(project)
+    } else {
+      log.info("Event while project is disposing/disposed")
     }
   }
 }
