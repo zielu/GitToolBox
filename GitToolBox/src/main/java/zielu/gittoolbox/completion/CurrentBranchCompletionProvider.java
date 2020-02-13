691d7e547267cdd6c4d08e2e30f5d23195748f15
Lukasz Zielinski <zieluuuu@gmail.com>
2020-02-13 23:31:40 +0100

    Gitmoji completion (#202)

--- GitToolBox/src/main/java/zielu/gittoolbox/completion/CurrentBranchCompletionProvider.java
+++ GitToolBox/src/main/java/zielu/gittoolbox/completion/CurrentBranchCompletionProvider.java
@@ -1,47 +1,25 @@
 package zielu.gittoolbox.completion;
 
-import com.google.common.collect.ImmutableMap;
-import com.intellij.codeInsight.completion.CompletionParameters;
-import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
-import com.intellij.util.BooleanFunction;
-import com.intellij.util.ProcessingContext;
 import git4idea.branch.GitBranchUtil;
 import git4idea.repo.GitRepository;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
-import zielu.gittoolbox.config.CommitCompletionMode;
-import zielu.gittoolbox.config.GitToolBoxConfig2;
-import zielu.gittoolbox.config.GitToolBoxConfigPrj;
 import zielu.gittoolbox.formatter.Formatted;
 import zielu.gittoolbox.formatter.Formatter;
 import zielu.gittoolbox.util.GtUtil;
 
-class CurrentBranchCompletionProvider extends CompletionProvider<CompletionParameters> {
-  private static final Map<CommitCompletionMode, BooleanFunction<CompletionParameters>> MODE_HANDLERS = ImmutableMap
-      .<CommitCompletionMode, BooleanFunction<CompletionParameters>>builder()
-      .put(CommitCompletionMode.AUTOMATIC, parameters -> true)
-      .put(CommitCompletionMode.ON_DEMAND, parameters -> !parameters.isAutoPopup())
-      .build();
+class CurrentBranchCompletionProvider extends CompletionProviderBase {
   private final Logger log = Logger.getInstance(getClass());
 
-  @Override
-  protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context,
-                                @NotNull CompletionResultSet result) {
-    if (shouldComplete(parameters)) {
-      setupCompletions(getProject(parameters), result);
-    }
-  }
-
-  private void setupCompletions(@NotNull Project project, @NotNull CompletionResultSet result) {
+  protected void setupCompletions(@NotNull Project project, @NotNull CompletionResultSet result) {
     CompletionService completionService = CompletionService.getInstance(project);
     List<Formatter> formatters = completionService.getFormatters();
     Collection<Pair<String, String>> branchInfos = getBranchInfo(completionService);
@@ -63,32 +41,8 @@ class CurrentBranchCompletionProvider extends CompletionProvider<CompletionParam
     }
   }
 
-  @NotNull
-  private Project getProject(@NotNull CompletionParameters parameters) {
-    return parameters.getPosition().getProject();
-  }
-
   private Collection<Pair<String, String>> getBranchInfo(@NotNull CompletionService completionService) {
     return completionService.getAffected().stream().map(getGitRepositoryNames()).collect(Collectors.toList());
-  }
-
-  private GitToolBoxConfigPrj getConfig(@NotNull CompletionParameters parameters) {
-    Project project = getProject(parameters);
-    return getConfig(project);
-  }
-
-  private GitToolBoxConfigPrj getConfig(@NotNull Project project) {
-    return GitToolBoxConfigPrj.getInstance(project);
-  }
-
-  private boolean shouldComplete(@NotNull CompletionParameters parameters) {
-    GitToolBoxConfigPrj projectConfig = getConfig(parameters);
-    if (projectConfig.getCommitDialogCompletion()) {
-      CommitCompletionMode mode = GitToolBoxConfig2.getInstance().getCommitDialogCompletionMode();
-      BooleanFunction<CompletionParameters> modeHandler = MODE_HANDLERS.getOrDefault(mode, params -> true);
-      return modeHandler.fun(parameters);
-    }
-    return false;
   }
 
   @NotNull
---
+++ GitToolBox/src/main/kotlin/zielu/gittoolbox/completion/gitmoji/GitmojiCompletionProvider.kt
@@ -0,0 +1,53 @@
+package zielu.gittoolbox.completion.gitmoji
+
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.InsertHandler
+import com.intellij.codeInsight.completion.InsertionContext
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.IconLoader
+import com.intellij.openapi.util.TextRange
+import zielu.gittoolbox.completion.CompletionProviderBase
+import zielu.gittoolbox.completion.CompletionService
+
+internal class GitmojiCompletionProvider : CompletionProviderBase() {
+  override fun setupCompletions(project: Project, result: CompletionResultSet) {
+    val completionService = CompletionService.getInstance(project)
+    if (completionService.affected.isNotEmpty()) {
+      addCompletions(result)
+    }
+  }
+
+  private fun addCompletions(result: CompletionResultSet) {
+    val insertHandler = PrefixCompletionInsertHandler()
+    GitmojiResBundle.keySet().forEach { gitmoji ->
+      val description = GitmojiResBundle.message(gitmoji)
+      val icon = IconLoader.getIcon("/zielu/gittoolbox/gitmoji/$gitmoji.png")
+      var builder = LookupElementBuilder.create(":$gitmoji:")
+        .withTypeText(description)
+        .withIcon(icon)
+        .withInsertHandler(insertHandler)
+
+      val wordsList = GitmojiKeywords.getKeywords(gitmoji)
+      if (wordsList.isNotEmpty()) {
+        builder = builder.withLookupStrings(wordsList)
+      }
+      result.addElement(builder)
+    }
+  }
+}
+
+private class PrefixCompletionInsertHandler() : InsertHandler<LookupElement> {
+  override fun handleInsert(context: InsertionContext, item: LookupElement) {
+    val gitmoji = item.lookupString
+    var startOffset = context.startOffset - 1
+    if (startOffset < 0) {
+      startOffset = 0
+    }
+    val textBeforeOffsets = context.document.getText(TextRange(startOffset, context.tailOffset))
+    if (textBeforeOffsets.startsWith(":$gitmoji")) {
+      context.document.replaceString(startOffset, context.tailOffset, gitmoji)
+    }
+  }
+}
