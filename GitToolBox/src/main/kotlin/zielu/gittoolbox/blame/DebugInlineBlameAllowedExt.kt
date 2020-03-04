b9c45bd4b019b81ce5800a8f56209aa858e80f48
Lukasz Zielinski <zieluuuu@gmail.com>
2020-03-04 19:09:11 +0100

    Optimize blame while debugging checks (#214)

--- GitToolBox/src/main/kotlin/zielu/gittoolbox/blame/DebugInlineBlameAllowedExt.kt
+++ GitToolBox/src/main/kotlin/zielu/gittoolbox/blame/DebugInlineBlameAllowedExt.kt
@@ -1,10 +1,13 @@
 package zielu.gittoolbox.blame
 
 import com.intellij.openapi.project.Project
+import zielu.gittoolbox.config.GitToolBoxConfig2
 import zielu.gittoolbox.extension.blame.InlineBlameAllowed
 
 internal class DebugInlineBlameAllowedExt : InlineBlameAllowed {
   override fun isAllowed(project: Project): Boolean {
-    return DebugInlineBlameAllowed.getInstance(project).isAllowed()
+    return if (GitToolBoxConfig2.getInstance().hideInlineBlameWhileDebugging) {
+      DebugInlineBlameAllowed.getInstance(project).isAllowed()
+    } else true
   }
 }
