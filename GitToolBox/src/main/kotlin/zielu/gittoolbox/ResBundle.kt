b5235eeee0b4f378e6a5bef26e775323ffdcbf87
Lukasz Zielinski <zieluuuu@gmail.com>
2020-05-04 23:36:27 +0200

    Kotlin migration

---
+++ GitToolBox/src/main/kotlin/zielu/gittoolbox/ResBundle.kt
@@ -0,0 +1,30 @@
+package zielu.gittoolbox
+
+import com.intellij.BundleBase
+import org.jetbrains.annotations.PropertyKey
+import zielu.intellij.util.ZBundleHolder
+
+internal object ResBundle {
+  private const val bundleName = "zielu.gittoolbox.ResourceBundle"
+  private val holder = ZBundleHolder(bundleName)
+
+  @JvmStatic
+  fun message(@PropertyKey(resourceBundle = bundleName) key: String, vararg params: Any?): String {
+    return BundleBase.message(holder.bundle, key, *params)
+  }
+
+  @JvmStatic
+  fun na(): String {
+    return message("common.na")
+  }
+
+  @JvmStatic
+  fun on(): String {
+    return message("common.on")
+  }
+
+  @JvmStatic
+  fun disabled(): String {
+    return message("common.disabled")
+  }
+}
