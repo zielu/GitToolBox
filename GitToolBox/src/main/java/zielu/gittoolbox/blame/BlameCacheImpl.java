e8b645fe58b41db944ebde311dc3264712bfc7f0
Lukasz Zielinski <zielu@users.noreply.github.com>
2020-08-25 11:10:23 +0200

    Improve plugin unload (#262)

--- GitToolBox/src/main/java/zielu/gittoolbox/blame/BlameCacheImpl.java
+++ GitToolBox/src/main/java/zielu/gittoolbox/blame/BlameCacheImpl.java
@@ -27,6 +27,7 @@ import zielu.gittoolbox.util.ExecutableTask;
 
 class BlameCacheImpl implements BlameCache, Disposable {
   private static final Logger LOG = Logger.getInstance(BlameCacheImpl.class);
+  private final AtomicBoolean active = new AtomicBoolean(true);
   private final BlameCacheLocalGateway gateway;
   private final LoadingCache<VirtualFile, Cached<Blamed>> annotations = CacheBuilder.newBuilder()
       .maximumSize(75)
@@ -43,14 +44,19 @@ class BlameCacheImpl implements BlameCache, Disposable {
 
   @Override
   public void dispose() {
-    annotations.invalidateAll();
-    queued.clear();
+    if (active.compareAndSet(true, false)) {
+      annotations.invalidateAll();
+      queued.clear();
+    }
   }
 
   @NotNull
   @Override
   public BlameAnnotation getAnnotation(@NotNull VirtualFile file) {
-    return gateway.getCacheGetTimer().timeSupplier(() -> getAnnotationInternal(file));
+    if (active.get()) {
+      return gateway.getCacheGetTimer().timeSupplier(() -> getAnnotationInternal(file));
+    }
+    return BlameAnnotation.EMPTY;
   }
 
   private BlameAnnotation getAnnotationInternal(@NotNull VirtualFile file) {
@@ -120,9 +126,15 @@ class BlameCacheImpl implements BlameCache, Disposable {
 
   @Override
   public void refreshForRoot(@NotNull VirtualFile root) {
+    if (active.get()) {
+      refreshForRootImpl(root);
+    }
+  }
+
+  private void refreshForRootImpl(@NotNull VirtualFile root) {
     LOG.debug("Refresh for root: ", root);
     Set<VirtualFile> files = new HashSet<>(annotations.asMap()
-                                               .keySet());
+        .keySet());
     files.stream()
         .filter(file -> VfsUtilCore.isAncestor(root, file, false))
         .forEach(this::markDirty);
@@ -139,10 +151,16 @@ class BlameCacheImpl implements BlameCache, Disposable {
 
   @Override
   public void invalidateForRoot(@NotNull VirtualFile root) {
+    if (active.get()) {
+      invalidateForRootImpl(root);
+    }
+  }
+
+  private void invalidateForRootImpl(@NotNull VirtualFile root) {
     LOG.debug("Invalidate for root: ", root);
     gateway.invalidateForRoot(root);
     Set<VirtualFile> files = new HashSet<>(annotations.asMap()
-                                               .keySet());
+        .keySet());
     files.stream()
         .filter(file -> VfsUtilCore.isAncestor(root, file, false))
         .forEach(this::invalidate);
--- GitToolBox/src/main/kotlin/zielu/gittoolbox/ui/projectview/ProjectViewSubscriber.kt
+++ GitToolBox/src/main/kotlin/zielu/gittoolbox/ui/projectview/ProjectViewSubscriber.kt
@@ -1,15 +1,29 @@
 package zielu.gittoolbox.ui.projectview
 
 import com.intellij.ide.projectView.ProjectView
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import zielu.gittoolbox.ui.util.AppUiUtil.invokeLater
 import zielu.gittoolbox.util.AppUtil
+import java.util.concurrent.atomic.AtomicBoolean
 
-internal class ProjectViewSubscriber(private val project: Project) {
+internal class ProjectViewSubscriber(
+  private val project: Project
+) : Disposable {
+  private val active = AtomicBoolean(true)
+
   fun refreshProjectView() {
-    invokeLater(project, Runnable {
-      ProjectView.getInstance(project).refresh()
-    })
+    if (active.get()) {
+      invokeLater(project, Runnable {
+        if (active.get()) {
+          ProjectView.getInstance(project).refresh()
+        }
+      })
+    }
+  }
+
+  override fun dispose() {
+    active.compareAndSet(true, false)
   }
 
   companion object {
