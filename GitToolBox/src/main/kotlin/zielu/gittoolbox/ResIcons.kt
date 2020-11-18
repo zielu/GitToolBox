9d4a49293a4399decf535ffbefde73fb0be94ef5
Lukasz Zielinski <zielu@users.noreply.github.com>
2020-11-18 23:13:48 +0100

    Replace deprecated API usage

--- GitToolBox/src/main/kotlin/zielu/gittoolbox/ResIcons.kt
+++ GitToolBox/src/main/kotlin/zielu/gittoolbox/ResIcons.kt
@@ -6,38 +6,40 @@ import javax.swing.Icon
 internal object ResIcons {
   @JvmStatic
   val BranchOrange: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/git-icon-orange.png")
+    get() = getIcon("/zielu/gittoolbox/git-icon-orange.png")
   @JvmStatic
   val BranchViolet: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/git-icon-violet.png")
+    get() = getIcon("/zielu/gittoolbox/git-icon-violet.png")
   @JvmStatic
   val Warning: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/exclamation-circle-frame.png")
+    get() = getIcon("/zielu/gittoolbox/exclamation-circle-frame.png")
   @JvmStatic
   val Error: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/exclamation-red-frame.png")
+    get() = getIcon("/zielu/gittoolbox/exclamation-red-frame.png")
   @JvmStatic
   val Ok: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/tick-circle-frame.png")
+    get() = getIcon("/zielu/gittoolbox/tick-circle-frame.png")
   @JvmStatic
   val Edit: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/edit.png")
+    get() = getIcon("/zielu/gittoolbox/edit.png")
   @JvmStatic
   val RegExp: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/regular-expression-search.png")
+    get() = getIcon("/zielu/gittoolbox/regular-expression-search.png")
   @JvmStatic
   val Commit: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/commit.png")
+    get() = getIcon("/zielu/gittoolbox/commit.png")
   @JvmStatic
   val Blame: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/git-icon-black.png")
+    get() = getIcon("/zielu/gittoolbox/git-icon-black.png")
   @JvmStatic
   val ChangesPresent: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/changes-present.svg")
+    get() = getIcon("/zielu/gittoolbox/changes-present.svg")
   @JvmStatic
   val NoChanges: Icon
-    get() = IconLoader.getIcon("/zielu/gittoolbox/changes-none.svg")
+    get() = getIcon("/zielu/gittoolbox/changes-none.svg")
   @JvmStatic
   val Logo: Icon
-    get() = IconLoader.getIcon("/META-INF/pluginIcon.svg")
+    get() = getIcon("/META-INF/pluginIcon.svg")
+
+  private fun getIcon(path: String) = IconLoader.getIcon(path, javaClass)
 }
