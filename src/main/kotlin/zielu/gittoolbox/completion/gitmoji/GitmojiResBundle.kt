eff7739ae55853accb4e1509d05c8b693aa1b91f
Lukasz Zielinski <zielu@users.noreply.github.com>
2020-12-03 23:57:33 +0100

    Fix: Exception when using the gitmoji completion (#281)

--- src/main/kotlin/zielu/gittoolbox/completion/gitmoji/GitmojiResBundle.kt
+++ src/main/kotlin/zielu/gittoolbox/completion/gitmoji/GitmojiResBundle.kt
@@ -4,16 +4,15 @@ import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
-internal object GitmojiResBundle {
-  @NonNls
-  private const val BUNDLE_NAME = "zielu.gittoolbox.gitmoji"
-  private val bundle = DynamicBundle(BUNDLE_NAME)
-
+internal object GitmojiResBundle : DynamicBundle(BUNDLE_NAME) {
   fun message(@PropertyKey(resourceBundle = BUNDLE_NAME) key: String, vararg params: Any?): String {
-    return bundle.getMessage(key, *params)
+    return getMessage(key, *params)
   }
 
   fun keySet(): Set<String> {
-    return bundle.resourceBundle.keySet()
+    return resourceBundle.keySet()
   }
 }
+
+@NonNls
+private const val BUNDLE_NAME = "zielu.gittoolbox.gitmoji"
