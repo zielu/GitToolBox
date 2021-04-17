f7ebd1e9207f7b28e7ece3912db69a5c706dc6ce
Lukasz Zielinski <zielu@users.noreply.github.com>
2021-04-17 21:47:29 +0200

    Minor refactoring (#305)

--- src/main/kotlin/zielu/gittoolbox/ui/statusbar/StatusActionGroupPopup.kt
+++ src/main/kotlin/zielu/gittoolbox/ui/statusbar/StatusActionGroupPopup.kt
@@ -4,23 +4,19 @@ import com.intellij.dvcs.ui.LightActionGroup
 import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.actionSystem.Separator
-import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Conditions
-import com.intellij.openapi.wm.IdeFocusManager
-import com.intellij.openapi.wm.WindowManager
 import com.intellij.ui.popup.PopupFactoryImpl.ActionGroupPopup
 import zielu.gittoolbox.ResBundle.message
 import zielu.gittoolbox.ui.statusbar.StatusBarActions.actionsFor
 import zielu.gittoolbox.ui.statusbar.actions.RefreshStatusAction
 import zielu.gittoolbox.ui.statusbar.actions.UpdateAction
+import zielu.gittoolbox.ui.util.AppUiUtil
 import zielu.gittoolbox.util.GtUtil
 import zielu.gittoolbox.util.GtUtil.getRepositories
 import zielu.gittoolbox.util.GtUtil.sort
-import java.awt.Component
 
 internal class StatusActionGroupPopup(
   title: String,
@@ -43,36 +39,7 @@ internal class StatusActionGroupPopup(
     private val log = Logger.getInstance(StatusActionGroupPopup::class.java)
 
     fun createDataContext(project: Project): DataContext {
-      val projectContext = SimpleDataContext.getProjectContext(project)
-      if (PlatformDataKeys.CONTEXT_COMPONENT.getData(projectContext) == null) {
-        log.warn("Missing context component, attempting a find it in project IdeFocusManager")
-        val prjFocusManager = IdeFocusManager.getInstance(project)
-        val prjComponent: Component? = prjFocusManager.focusOwner
-
-        return if (prjComponent != null) {
-          wrapComponentIntoContext(prjComponent, projectContext)
-        } else {
-          log.warn("Missing project focus owner, use project frame")
-          val projectFrame: Component? = WindowManager.getInstance().getFrame(project)
-          if (projectFrame != null) {
-            wrapComponentIntoContext(projectFrame, projectContext)
-          } else {
-            log.warn("Missing context component")
-            projectContext
-          }
-        }
-      } else {
-        return projectContext
-      }
-    }
-
-    private fun wrapComponentIntoContext(component: Component, parent: DataContext): DataContext {
-      return SimpleDataContext.getSimpleContext(
-        mapOf(
-          PlatformDataKeys.CONTEXT_COMPONENT.name to component
-        ),
-        parent
-      )
+      return AppUiUtil.prepareDataContextWithContextComponent(project)
     }
 
     fun createActions(project: Project): ActionGroup {
--- src/main/kotlin/zielu/gittoolbox/ui/util/AppUiUtil.kt
+++ src/main/kotlin/zielu/gittoolbox/ui/util/AppUiUtil.kt
@@ -1,14 +1,23 @@
 package zielu.gittoolbox.ui.util
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 import com.intellij.openapi.application.Application
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.wm.IdeFocusManager
+import com.intellij.openapi.wm.WindowManager
 import zielu.gittoolbox.GitToolBoxPrj
 import zielu.intellij.concurrent.ZDisposableRunnableWrapper
+import java.awt.Component
 
 internal object AppUiUtil {
+  private val log = Logger.getInstance(AppUiUtil::class.java)
+
   @JvmStatic
   fun invokeLater(task: Runnable) {
     val application = getApplication()
@@ -51,6 +60,40 @@ internal object AppUiUtil {
     val toDo = ZDisposableRunnableWrapper(task)
     Disposer.register(disposable, toDo)
     invokeLaterIfNeeded(toDo)
+  }
+
+  fun prepareDataContextWithContextComponent(project: Project): DataContext {
+    var component: Component? = IdeFocusManager.getInstance(project).focusOwner
+
+    if (component == null) {
+      log.info("Missing project focus owner, find global one")
+      component = IdeFocusManager.getGlobalInstance().focusOwner
+    }
+    if (component == null) {
+      log.warn("Missing global focus owner, find project frame")
+      component = WindowManager.getInstance().getFrame(project)
+    }
+    if (component == null) {
+      log.warn("Missing project frame, find visible frame")
+      component = WindowManager.getInstance().findVisibleFrame()
+    }
+    if (component == null) {
+      log.warn("Missing context component")
+    }
+    return contextWithComponent(SimpleDataContext.getProjectContext(project), component)
+  }
+
+  private fun contextWithComponent(parent: DataContext, component: Component?): DataContext {
+    if (component == null) {
+      return parent
+    }
+
+    return SimpleDataContext.getSimpleContext(
+      mapOf(
+        PlatformDataKeys.CONTEXT_COMPONENT.name to component
+      ),
+      parent
+    )
   }
 
   private fun getApplication(): Application {
